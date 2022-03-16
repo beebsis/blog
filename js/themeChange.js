b156f261162e139db84a2cbfa4b4a5b8a0d2392d
LawTRIBAL-Official <65894260+LawTRIBAL-Official@users.noreply.github.com>
2022-03-16 06:57:22 +0100

    Initial commit

---
+++ js/themeChange.js
@@ -0,0 +1,24 @@
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+const toggleSwitch = document.querySelector('.theme-switch');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //this will be set to dark
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //this will be set to light
+    } 
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
