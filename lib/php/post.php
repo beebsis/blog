f54b11d094fb30a29c2da4887945d01abf743eaa
LawTRIBAL-Official <65894260+LawTRIBAL-Official@users.noreply.github.com>
2022-04-19 11:01:58 +0200

    blog updated

---
+++ lib/php/post.php
@@ -0,0 +1,22 @@
+<html lang='en'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <script src='https://kit.fontawesome.com/bb5aae4646.js' crossorigin='anonymous'></script>
+        <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.15.4/css/all.css' integrity='sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm' crossorigin='anonymous'>
+        <!--Utility-->
+        <link rel='stylesheet' href='./css/utility/reset.css'>
+        <link rel='stylesheet' href='./css/utility/colors.css'>
+        <link rel='stylesheet' href='./css/utility/fonts.css'>
+        <link rel='stylesheet' href='./css/utility/ezCSS.css'>
+        <!--page-->
+        <link rel='stylesheet' href='./css/index.css'>
+        <!--JavaScript-->
+        
+        <title>Add Post</title>
+    </head>
+    <body>
+
+    </body>
+</html>
